fd7dd2b8bbd64b112b4b3b356781eb68b3a317a9
pedroluca <pedrolucaofc@outlook.com>
2023-07-12 07:28:03 -0300

    feat: implemented new document title feature

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             <Route path="/projetos" element={<Projects/>}/>
             <Route path="/setup" element={<Setup/>}/>
             <Route path="/contact" element={<Contact/>}/>
-            <Route path="/contatos" element={<Contact/>}/>
+            <Route path="/contate-me" element={<Contact/>}/>
           </Routes>
         </BrowserRouter>
       </div>
