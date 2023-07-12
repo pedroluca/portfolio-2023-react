6bc3c17ea911db0ddd9a4f0337f856cdd8aa775f
pedroluca <pedrolucaofc@outlook.com>
2023-07-12 06:21:48 -0300

    feat: auto calc of my age

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,13 +3,19 @@ import Pedro from '../../images/profile.jpg'
 import PageTitle from '../../components/PageTitle/PageTitle'
 
 function About() {
+
+  const todayDate = new Date()
+  const myBirthday = new Date('2003-01-28')
+  const differenceInMilliseconds = todayDate - myBirthday
+  const myAge = parseInt(differenceInMilliseconds / (1000 * 60 * 60 * 24 * 365.25))
+
   return (
     <div className="page page-column">
       <PageTitle name="Sobre mim" />
       <section id="about">
         <img src={Pedro} alt="Pedro is using a white shirt from her college's team, and wearing sunglasses"/>
         <div>
-          <p>Meu nome é Pedro Luca, tenho 20 anos e sou Técnico em Informática.</p>
+          <p>Meu nome é Pedro Luca, tenho {myAge} anos e sou Técnico em Informática.</p>
           <p>Em 2021, conclui o curso técnico em Informática para Internet no IF Baiano - campus Guanambi, onde atualmente estou cursando a faculdade de Análise e Desenvolvimento de Sistemas.</p>
           <p>Sou apaixonado pelo mundo da programação desde que iniciei os estudos sobre isso em 2018, e me identifico demais com a área do Front-End.</p>
           <p>Tenho experiência com as tecnologias: HTML, CSS, PHP, SQL, C e um conhecimento básico de Javascript.</p>
