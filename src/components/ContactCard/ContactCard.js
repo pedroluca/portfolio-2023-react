5df938d03576aed817741919d19c9e226d00b1d6
pedroluca <pedrolucaofc@outlook.com>
2023-10-02 11:07:34 -0300

    fix: changed new X image path

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -2,7 +2,7 @@ import './ContactCard.css'
 import Instagram from '../../images/instagram.png'
 import Github from '../../images/github.png'
 import Linkedin from '../../images/linkedin.png'
-import Twitter from '../../images/twitter.png'
+import Twitter from '../../images/xtwitter.png'
 import Youtube from '../../images/youtube.png'
 import Twitch from '../../images/twitch.png'
 import Outlook from '../../images/outlook.png'
@@ -16,7 +16,7 @@ function ContactCard(props) {
   if (props.src === 'instagram') imagePath = Instagram
   else if (props.src === 'github') imagePath = Github
   else if (props.src === 'linkedin') imagePath = Linkedin
-  else if (props.src === 'twitter') imagePath = Twitter
+  else if (props.src === 'xtwitter') imagePath = Twitter
   else if (props.src === 'youtube') imagePath = Youtube
   else if (props.src === 'twitch') imagePath = Twitch
   else if (props.src === 'outlook') imagePath = Outlook
