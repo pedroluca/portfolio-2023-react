fe86e3f78db1bb972b22e0ebcff63075a9e8aed1
pedroluca <pedrolucaofc@outlook.com>
2023-10-03 08:30:15 -0300

    feat: changed some stuff in contact card style

--- src/components/ContactCard/ContactCard.js
+++ src/components/ContactCard/ContactCard.js
@@ -2,31 +2,29 @@ import './ContactCard.css'
 import Instagram from '../../images/instagram.png'
 import Github from '../../images/github.png'
 import Linkedin from '../../images/linkedin.png'
-import Twitter from '../../images/xtwitter.png'
+import Xtwitter from '../../images/xtwitter.png'
 import Youtube from '../../images/youtube.png'
 import Twitch from '../../images/twitch.png'
 import Outlook from '../../images/outlook.png'
 
 function ContactCard(props) {
-  const handleCardClick = () => {
-    window.open(props.url, 'blank')
-  }
+  const handleCardClick = () => window.open(props.url, 'blank')
   
   let imagePath
   if (props.src === 'instagram') imagePath = Instagram
   else if (props.src === 'github') imagePath = Github
   else if (props.src === 'linkedin') imagePath = Linkedin
-  else if (props.src === 'xtwitter') imagePath = Twitter
+  else if (props.src === 'xtwitter') imagePath = Xtwitter
   else if (props.src === 'youtube') imagePath = Youtube
   else if (props.src === 'twitch') imagePath = Twitch
   else if (props.src === 'outlook') imagePath = Outlook
-  
+
   return (
     <div className="contact-card" onClick={handleCardClick}>
       <img src={imagePath} alt="Social media specified logo" />
       <h4>{props.social}</h4>
       <p>{props.user}</p>
-      <img src={imagePath} alt="Social media specified logo" id="hidden-img" />
+      <img src={imagePath} alt="Hidden logo" id="hidden-img" />
     </div>
   )
 }
---
+++ src/components/SetupItems/SetupItems.js
@@ -0,0 +1,55 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import './SetupItems.css'
+import JetBrains from '../../images/jetbrains.png'
+import Omni from '../../images/omni.png'
+import VSCode from '../../images/vscode.png'
+import Laptop from '../../images/laptop.png'
+import Headset from '../../images/headset.png'
+import Headphone from '../../images/headphone.png'
+import Earpods1 from '../../images/earphone1.png'
+import Earpods2 from '../../images/earphone2.png'
+import Mouse1 from '../../images/mouse1.png'
+import Mouse2 from '../../images/mouse2.png'
+import Mousepad from '../../images/mousepad.png'
+import CodigoLimpo from '../../images/codigo.png'
+import Arquitetura from '../../images/arquitetura.png'
+import PHP4 from '../../images/php4.jpg'
+import DevAndroid from '../../images/dev_android.jpg'
+import EstruturaDados from '../../images/estrutura_dados.jpg'
+import Deschamps from '../../images/deschamps.png'
+import TheNews from '../../images/the_news.png'
+
+function ContactCard(props) {
+  const handleCardClick = () => window.open(props.url, 'blank')
+  
+  let imagePath
+  if (props.src === 'laptop') imagePath = Laptop
+  else if (props.src === 'mouse1') imagePath = Mouse1
+  else if (props.src === 'mouse2') imagePath = Mouse2
+  else if (props.src === 'earphone1') imagePath = Earpods1
+  else if (props.src === 'earphone2') imagePath = Earpods2
+  else if (props.src === 'headphone') imagePath = Headphone
+  else if (props.src === 'headset') imagePath = Headset
+  else if (props.src === 'mousepad') imagePath = Mousepad
+  else if (props.src === 'omni') imagePath = Omni
+  else if (props.src === 'jetbrains') imagePath = JetBrains
+  else if (props.src === 'vscode') imagePath = VSCode
+  else if (props.src === 'codigo') imagePath = CodigoLimpo
+  else if (props.src === 'arquitetura') imagePath = Arquitetura
+  else if (props.src === 'php4') imagePath = PHP4
+  else if (props.src === 'estrutura') imagePath = EstruturaDados
+  else if (props.src === 'android') imagePath = DevAndroid
+  else if (props.src === 'deschamps') imagePath = Deschamps
+  else if (props.src === 'thenews') imagePath = TheNews
+
+  return (
+    <div className="contact-card" onClick={handleCardClick}>
+      <img className="img-contain" src={imagePath} alt="Setup item image for recognition" />
+      <h4>{props.social}</h4>
+      <p>{props.user}</p>
+      <img src={imagePath} alt="Hidden image just for design" id="hidden-img" />
+    </div>
+  )
+}
+
+export default ContactCard
