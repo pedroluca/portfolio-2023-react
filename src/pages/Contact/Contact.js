d4081337c47fd131b83d0e80f0928aad968b2046
pedroluca <pedrolucaofc@outlook.com>
2023-10-03 09:01:14 -0300

    fix: changed files name

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import './Contact.css'
 import PageTitle from '../../components/PageTitle/PageTitle'
-import ContactCard from '../../components/ContactCard/ContactCard'
+import LinkCard from '../../components/LinkCard/LinkCard'
 import DocumentTitle from '../../components/DocumentTitle/DocumentTitle'
 
 function Contact() {
@@ -27,13 +27,13 @@ function Contact() {
       <DocumentTitle title="Meus Links" />
       <PageTitle name="Meus Links" />
       <section>
-        <ContactCard src="instagram" url="https://www.instagram.com/pe.uuh" social="Instagram" user="@pe.uuh" />
-        <ContactCard src="xtwitter" url="https://www.twitter.com/pedrolucaofc" social="Twitter" user="@pedrolucaofc" />
-        <ContactCard src="github" url={githubLink} social="Github" user={data && data.user.login} />
-        <ContactCard src="linkedin" url="https://www.linkedin.com/in/pedro-luca-prates" social="Linkedin" user="Pedro Luca Prates" />
-        <ContactCard src="outlook" url="mailto: pedrolucaofc@outlook.com" social="Outlook" user="pedrolucaofc" />
-        <ContactCard src="youtube" url="https://www.youtube.com/channel/UC7qDaMEUS4MjhQ7UVxQQVMQ" social="YouTube" user="Pedro Luca Prates" />
-        <ContactCard src="twitch" url="https://www.twitch.com/PedroLucaOFC" social="Twitch" user="PedroLucaOFC" />
+        <LinkCard src="instagram" url="https://www.instagram.com/pe.uuh" social="Instagram" user="@pe.uuh" />
+        <LinkCard src="xtwitter" url="https://www.twitter.com/pedrolucaofc" social="Twitter" user="@pedrolucaofc" />
+        <LinkCard src="github" url={githubLink} social="Github" user={data && data.user.login} />
+        <LinkCard src="linkedin" url="https://www.linkedin.com/in/pedro-luca-prates" social="Linkedin" user="Pedro Luca Prates" />
+        <LinkCard src="outlook" url="mailto: pedrolucaofc@outlook.com" social="Outlook" user="pedrolucaofc" />
+        <LinkCard src="youtube" url="https://www.youtube.com/channel/UC7qDaMEUS4MjhQ7UVxQQVMQ" social="YouTube" user="Pedro Luca Prates" />
+        <LinkCard src="twitch" url="https://www.twitch.com/PedroLucaOFC" social="Twitch" user="PedroLucaOFC" />
       </section>
     </main>
   )
--- src/pages/Setup/Setup.js
+++ src/pages/Setup/Setup.js
@@ -1,7 +1,7 @@
 import './Setup.css'
 import PageTitle from '../../components/PageTitle/PageTitle'
 import DocumentTitle from '../../components/DocumentTitle/DocumentTitle'
-import SetupItems from '../../components/SetupItems/SetupItems'
+import SetupCard from '../../components/SetupCard/SetupCard'
 
 function Setup() {
   return (
@@ -12,39 +12,39 @@ function Setup() {
         <div className="setup-area">
           <h3>Workstation</h3>
           <div className="setup-holder">
-            <SetupItems src="laptop" social="Laptop" user="Lenovo L340 Gaming" url="https://www.lenovo.com/br/pt/laptops/ideapad/ideapad-gaming-laptops/IdeaPad-L340-15IRH-Gaming/p/88IPL301161" />
-            <SetupItems src="headset" social="Headset" user="Razer Kraken Lite" url="https://www.amazon.com.br/gp/product/B07XC936P8/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1" />
-            <SetupItems src="headphone" social="Headphone" user="Philips UH202 BT" url="https://www.amazon.com.br/gp/product/B07YNG2L67/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&th=1" />
-            <SetupItems src="earphone1" social="Earpods" user="Baseus Encok W04" url="https://www.amazon.com.br/gp/product/B01LYIY2HG/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1" />
-            <SetupItems src="earphone2" social="Earpods" user="Lenovo Thinkplus LP40" url="https://pt.aliexpress.com/item/1005004866196078.html?spm=a2g0o.order_list.order_list_main.5.6d4bcaa4daN1Nt&gatewayAdapt=glo2bra" />
-            <SetupItems src="mouse1" social="Mouse" user="Logitech M170 Wireless" url="https://www.amazon.com.br/Mouse-Nanoreceptor-Inclusa-Logitech-Mouses/dp/B074L9L5KZ?th=1" />
-            <SetupItems src="mouse2" social="Mouse" user="Baseus F01B Wireless" url="https://pt.aliexpress.com/item/1005005847298347.html?spm=a2g0o.order_list.order_list_main.6.649ecaa4iWc5Ig&gatewayAdapt=glo2bra" />
-            <SetupItems src="mousepad" social="Mousepad" user="Fortrek G Speed Medium" url="https://www.amazon.com.br/gp/product/B0765YY6TM/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&th=1" />
+            <SetupCard src="laptop" social="Laptop" user="Lenovo L340 Gaming" url="https://www.lenovo.com/br/pt/laptops/ideapad/ideapad-gaming-laptops/IdeaPad-L340-15IRH-Gaming/p/88IPL301161" />
+            <SetupCard src="headset" social="Headset" user="Razer Kraken Lite" url="https://www.amazon.com.br/gp/product/B07XC936P8/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1" />
+            <SetupCard src="headphone" social="Headphone" user="Philips UH202 BT" url="https://www.amazon.com.br/gp/product/B07YNG2L67/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&th=1" />
+            <SetupCard src="earphone1" social="Earpods" user="Baseus Encok W04" url="https://www.amazon.com.br/gp/product/B01LYIY2HG/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1" />
+            <SetupCard src="earphone2" social="Earpods" user="Lenovo Thinkplus LP40" url="https://pt.aliexpress.com/item/1005004866196078.html?spm=a2g0o.order_list.order_list_main.5.6d4bcaa4daN1Nt&gatewayAdapt=glo2bra" />
+            <SetupCard src="mouse1" social="Mouse" user="Logitech M170 Wireless" url="https://www.amazon.com.br/Mouse-Nanoreceptor-Inclusa-Logitech-Mouses/dp/B074L9L5KZ?th=1" />
+            <SetupCard src="mouse2" social="Mouse" user="Baseus F01B Wireless" url="https://pt.aliexpress.com/item/1005005847298347.html?spm=a2g0o.order_list.order_list_main.6.649ecaa4iWc5Ig&gatewayAdapt=glo2bra" />
+            <SetupCard src="mousepad" social="Mousepad" user="Fortrek G Speed Medium" url="https://www.amazon.com.br/gp/product/B0765YY6TM/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&th=1" />
           </div>
         </div>
         <div className="setup-area">
           <h3>Coding</h3>
           <div className="setup-holder">
-            <SetupItems src="vscode" social="IDE" user="VS Code" url="https://code.visualstudio.com/" />
-            <SetupItems src="omni" social="Tema" user="Omni" url="https://marketplace.visualstudio.com/items?itemName=rocketseat.theme-omni" />
-            <SetupItems src="jetbrains" social="Fonte" user="JetBrains Mono" url="https://www.jetbrains.com/pt-br/lp/mono/" />
+            <SetupCard src="vscode" social="IDE" user="VS Code" url="https://code.visualstudio.com/" />
+            <SetupCard src="omni" social="Tema" user="Omni" url="https://marketplace.visualstudio.com/items?itemName=rocketseat.theme-omni" />
+            <SetupCard src="jetbrains" social="Fonte" user="JetBrains Mono" url="https://www.jetbrains.com/pt-br/lp/mono/" />
           </div>
         </div>
         <div className="setup-area">
           <h3>Books</h3>
           <div className="setup-holder tooltip-container">
-            <SetupItems src="codigo" social="Código Limpo" url="https://www.amazon.com.br/gp/product/8576082675/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1" />
-            <SetupItems src="arquitetura" social="Arquitetura Limpa" url="https://www.amazon.com.br/gp/product/8550804606/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1" />
-            <SetupItems src="php4" social="PHP4 e MySQL" url="/setup" />
-            <SetupItems src="estrutura" social="Estrutura de Dados" url="/setup" />
-            <SetupItems src="android" social="Aplicativos para o Android" url="/setup" />
+            <SetupCard src="codigo" social="Código Limpo" url="https://www.amazon.com.br/gp/product/8576082675/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1" />
+            <SetupCard src="arquitetura" social="Arquitetura Limpa" url="https://www.amazon.com.br/gp/product/8550804606/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1" />
+            <SetupCard src="php4" social="PHP4 e MySQL" url="/setup" />
+            <SetupCard src="estrutura" social="Estrutura de Dados" url="/setup" />
+            <SetupCard src="android" social="Aplicativos para o Android" url="/setup" />
           </div>
         </div>
         <div className="setup-area">
           <h3>Newsletter</h3>
           <div className="setup-holder">
-            <SetupItems src="deschamps" social="Filipe Deschamps" url="https://filipedeschamps.com.br/newsletter" />
-            <SetupItems src="thenews" social="The News" url="https://thenewscc.com.br/indicacao/?grsf=vlch08" />
+            <SetupCard src="deschamps" social="Filipe Deschamps" url="https://filipedeschamps.com.br/newsletter" />
+            <SetupCard src="thenews" social="The News" url="https://thenewscc.com.br/indicacao/?grsf=vlch08" />
           </div>
         </div>
       </section>
