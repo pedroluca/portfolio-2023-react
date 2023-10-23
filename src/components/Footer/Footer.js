c96c9a0f4e425997841e737ef0545d7f418055b2
pedroluca <pedrolucaofc@outlook.com>
2023-10-23 11:19:45 -0300

    feat: new social media logos for footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,21 @@
 import './Footer.css'
-import Instagram from '../../images/instagram.png'
-import Github from '../../images/github.png'
-import Linkedin from '../../images/linkedin.png'
-import Xtwitter from '../../images/xtwitter.png'
-import Youtube from '../../images/youtube.png'
-import Twitch from '../../images/twitch.png'
+import Instagram from '../../images/linear-instagram-darkgray-888.png'
+import Github from '../../images/linear-github-darkgray-888.png'
+import Linkedin from '../../images/linear-linkedin-darkgray-888.png'
+import Xtwitter from '../../images/linear-X-darkgray-888.png'
+import Youtube from '../../images/linear-youtube-darkgray-888.png'
+import Twitch from '../../images/linear-twitch-darkgray-888.png'
 
 function Footer() {
   return (
     <div className="footer-bar">
       <nav>
-        <a href="https://www.instagram.com/pe.uuh" target="blank" id="instagram"><span>instagram</span><img src={Instagram} alt="Instagram logo"/></a>
-        <a href="https://www.github.com/pedroluca" target="blank" id="github"><span>github</span><img src={Github} alt="Github logo"/></a>
-        <a href="https://www.linkedin.com/in/pedro-luca-prates" target="blank" id="linkedin"><span>linkedin</span><img src={Linkedin} alt="Linkedin logo"/></a>
-        <a href="https://www.twitter.com/pedrolucaofc" target="blank" id="twitter"><span>X / twitter</span><img src={Xtwitter} alt="X logo"/></a>
-        <a href="https://www.youtube.com/channel/UC7qDaMEUS4MjhQ7UVxQQVMQ/" target="blank" id="youtube"><span>youtube</span><img src={Youtube} alt="Youtube logo"/></a>
-        <a href="https://www.twitch.com/pedrolucaofc" target="blank" id="twitch"><span>twitch</span><img src={Twitch} alt="Twitch logo"/></a>
+        <a href="https://www.instagram.com/pe.uuh" target="blank" id="instagram"><img src={Instagram} alt="Instagram logo"/></a>
+        <a href="https://www.github.com/pedroluca" target="blank" id="github"><img src={Github} alt="Github logo"/></a>
+        <a href="https://www.linkedin.com/in/pedro-luca-prates" target="blank" id="linkedin"><img src={Linkedin} alt="Linkedin logo"/></a>
+        <a href="https://www.twitter.com/pedrolucaofc" target="blank" id="twitter"><img src={Xtwitter} alt="X logo"/></a>
+        <a href="https://www.youtube.com/channel/UC7qDaMEUS4MjhQ7UVxQQVMQ/" target="blank" id="youtube"><img src={Youtube} alt="Youtube logo"/></a>
+        <a href="https://www.twitch.com/pedrolucaofc" target="blank" id="twitch"><img src={Twitch} alt="Twitch logo"/></a>
       </nav>
     </div>
   )
